1ca89fd200c7828375ad811916c28fcdd0c5a538
ThilinaJayamal <thilinajayamal271@gmail.com>
2024-08-09 13:02:26 +0530

    rename screen components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const FeedScreen = () => {
   return (
     <View>
       <Text>Feed</Text>
@@ -9,4 +9,4 @@ const index = () => {
   )
 }
 
-export default index
+export default FeedScreen
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Profile = () => {
+const ProfileScreen = () => {
   return (
     <View>
       <Text>Profile</Text>
@@ -9,4 +9,4 @@ const Profile = () => {
   )
 }
 
-export default Profile
+export default ProfileScreen
