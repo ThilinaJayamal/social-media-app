01fb637058f0223a958ade0c2616c9070bc7d541
ThilinaJayamal <thilinajayamal271@gmail.com>
2024-08-10 15:47:33 +0530

    add image picker

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,15 +1,37 @@
 import { View, Text, Image, TextInput, Pressable } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import * as ImagePicker from 'expo-image-picker';
 
 const AddPost = () => {
-
   const [caption, setCaption] = useState("");
+  const [image, setImage] = useState<undefined | string>(undefined);
+
+  useEffect(() => {
+    if(!image){
+      pickImage();
+    }
+  }, [image])
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [3, 4],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View className='p-3 items-center flex-1 justify-between'>
 
       <View className='w-full items-center'>
-        <Image source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg" }} className='w-52 aspect-[3/4] rounded-xl' />
-        <Text className='text-blue-500 font-semibold m-5' onPress={() => { }}>Change</Text>
+        <Image source={{ uri:image }} className='w-52 aspect-[3/4] rounded-xl bg-slate-300' />
+        <Text className='text-blue-500 font-semibold m-5' onPress={pickImage}>Change</Text>
         <TextInput placeholder="What's in your mind" className='w-full p-3' value={caption} onChangeText={(newCaption) => setCaption(newCaption)} />
       </View>
 
