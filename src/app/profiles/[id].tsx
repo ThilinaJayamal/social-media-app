eefacb001bb969e2bc389f0ab7dfcea5b451b400
ThilinaJayamal <thilinajayamal271@gmail.com>
2024-08-25 09:50:10 +0530

    setup hyperlink

--- src/app/profiles/[id].tsx
+++ src/app/profiles/[id].tsx
@@ -1,11 +1,12 @@
 import { View, Text, Alert, Image, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { router, useLocalSearchParams } from 'expo-router'
+import { Link, useLocalSearchParams } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { supabase } from '@/src/lib/supabase';
 import { ImageUrlGenerator } from '../(tabs)/search';
 import { useAuth } from '@/src/providers/AuthProvider';
 import Button from '@/src/components/Button';
+import PostListItem from '@/src/components/PostListItem';
 
 const UserProfile = () => {
 
@@ -62,62 +63,72 @@ const UserProfile = () => {
 
     return (
         <SafeAreaView>
-            <FlatList 
-            showsVerticalScrollIndicator={false}
-            contentContainerClassName='p-5' 
-            data={posts} renderItem={({ item }) => (
-                <View className='mb-5'>
-                    <Image source={{ uri: ImageUrlGenerator(item.image, 250) }} className='w-full aspect-square rounded-xl' />
-                </View>
-            )} ListHeaderComponent={
-                <View className='items-center mb-8'>
-
-                    <View className='bg-blue-500 h-52 w-full rounded-t-3xl' />
-
-                    <View className='mt-[-104]'>
-                        <Image source={{ uri: ImageUrlGenerator(avatarUrl, 250) }} className='w-52 h-52 rounded-full border border-gray-300' />
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                data={posts} renderItem={({ item }) => (
+                    <View className='mb-5'>
+                        <PostListItem post={item} />
                     </View>
+                )} ListHeaderComponent={
+                    <View className='items-center mb-8 p-5'>
 
-                    <View className='w-full mt-5'>
-                        <View className='flex-row justify-between items-baseline'>
-                            <View className='border-r-2 pr-4'>
-                                <Text className='font-semibold text-lg'>{name}</Text>
-                                <Text className='text-gray-600 text-base'>{username}</Text>
-                            </View>
+                        <View className='bg-blue-500 h-52 w-full rounded-t-3xl' />
 
-                            <View>
-                                <Text className='font-semibold text-xl text-center'>{posts?.length}</Text>
-                                <Text className='text-gray-800'>post</Text>
-                            </View>
-
-                            <View>
-                                <Text className='font-semibold text-xl text-center'>0</Text>
-                                <Text className='text-gray-800 '>following</Text>
-                            </View>
-
-                            <View>
-                                <Text className='font-semibold text-xl text-center'>0</Text>
-                                <Text className='text-gray-800 '>followers</Text>
-                            </View>
+                        <View className='mt-[-104]'>
+                            <Image source={{ uri: ImageUrlGenerator(avatarUrl, 250) }} className='w-52 h-52 rounded-full border border-gray-300' />
                         </View>
 
-                        <Text className='mt-4 text-base'>{bio}</Text>
-
-                        {
-                            (user?.id.toString() === id.toString()) ?
-                                <View className='mt-4'>
-                                    <Button title='Edit Profile' onPress={() => router.push("/(tabs)/profile")} />
+                        <View className='w-full mt-5'>
+                            <View className='flex-row justify-between items-baseline'>
+                                <View className='border-r-2 pr-4'>
+                                    <Text className='font-semibold text-lg'>{name}</Text>
+                                    <Text className='text-gray-600 text-base'>{username}</Text>
                                 </View>
-                                :
-                                <View className='mt-4 w-2/4'>
-                                    <Button title='Follow' onPress={() => router.push("/(tabs)/profile")} />
-                                </View>
-                        }
 
+                                <View>
+                                    <Text className='font-semibold text-xl text-center'>{posts?.length}</Text>
+                                    <Text className='text-gray-800'>post</Text>
+                                </View>
+
+                                <View>
+                                    <Text className='font-semibold text-xl text-center'>0</Text>
+                                    <Text className='text-gray-800 '>following</Text>
+                                </View>
+
+                                <View>
+                                    <Text className='font-semibold text-xl text-center'>0</Text>
+                                    <Text className='text-gray-800 '>followers</Text>
+                                </View>
+                            </View>
+
+                            <Text className='mt-4 text-base'>{bio}</Text>
+
+                            {
+                                (user?.id.toString() === id.toString()) ?
+                                    <View className='mt-4 flex-row gap-5'>
+                                        <View className='w-1/3'>
+                                            <Link href={"/(tabs)/profile"} asChild>
+                                                <Button title='Edit Profile' />
+                                            </Link>
+                                        </View>
+                                        <View className='w-1/3'>
+                                            <Link href={"/(tabs)/new"} asChild>
+                                                <Button title='+ ADD' />
+                                            </Link>
+                                        </View>
+                                    </View>
+                                    :
+                                    <View className='mt-4 w-2/4'>
+                                        <Link href={"/(tabs)/new"} asChild>
+                                            <Button title='Follow' />
+                                        </Link>
+                                    </View>
+                            }
+
+                        </View>
                     </View>
-                </View>
 
-            } />
+                } />
 
         </SafeAreaView>
     )
